0adaae3278361c8112506b89550c98423d7835ac
Prathamesh Gawade <prathamesh.gawade82@gmail.com>
2019-12-05 12:17:20 +0530

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -80,7 +80,8 @@ function Ensure-User() {
     $User = $null;
     try
     {
-        $Claim = New-SPClaimsPrincipal -TrustedIdentityTokenIssuer "ADFS Staging" -Identity $UserLoginName;
+        [string]$TrustedIdentityTokenIssuer = "ADFS Staging"
+        $Claim = New-SPClaimsPrincipal -TrustedIdentityTokenIssuer $TrustedIdentityTokenIssuer -Identity $UserLoginName;
         $User = $Web.EnsureUser($claim.ToEncodedString());        
     }
     catch
@@ -91,6 +92,7 @@ function Ensure-User() {
     return $User;
 }
 
+#Get Sharepoint list items using CAML Query
 function Get-SPListItems() {
     param (
         [Parameter(Mandatory = $true)]
@@ -126,5 +128,270 @@ function Get-SPListItems() {
     }
 
     return $ListItems;
+}
+
+function Assign-Unique-Permissions-On-Folder() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPFolder]
+        $SPFolder
+    )
+
+    $Processed = $true
+
+    try
+    {
+        if($SPFolder.Item.HasUniqueRoleAssignments -eq $false)
+        {
+            $SPFolder.Item.BreakRoleInheritance($false)
+        }
+    }
+    catch
+    {
+        $Processed = $false
+
+        $message = "Failed while assigning unique permissions to folder.Error: " + $_.Exception.Message 
+        Write-To-Console -message $message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.ERROR
+
+    }
+
+    return $Processed
+}
+
+function Assign-Unique-Permissions-On-List() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPList]
+        $SPList
+    )
+
+    $Processed = $true
+
+    try
+    {
+        if($SPList.HasUniqueRoleAssignments -eq $false)
+        {
+            $SPList.BreakRoleInheritance($false)
+        }
+    }
+    catch
+    {
+        $Processed = $false
+
+        $message = "Failed while assigning unique permissions to list: '" + $SPList.Title + "'.Error: " + $_.Exception.Message 
+        Write-To-Console -message $message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.ERROR
+
+    }
+
+    return $Processed
+}
+
+function Create-SPGroup() {
+    param(
+       [Parameter(Mandatory = $true)]
+       [Microsoft.SharePoint.SPWeb]
+       $SPWeb,
+       
+       [Parameter(Mandatory = $true)]
+       [string]
+       $Group_Name,
+       
+       [Parameter(Mandatory = $true)]
+       [Microsoft.SharePoint.SPUser]
+       $Owner,
+
+       [Parameter(Mandatory = $true)]
+       [Microsoft.SharePoint.SPUser]
+       [AllowNull()]
+       $Default_Member,
+
+       [Parameter(Mandatory = $true)]
+       [string]
+       $Description
+    )
+
+    $SPGroup = $null
+
+    try
+    {
+        $SPGroup = $SPWeb.SiteGroups[$Group_Name]
+
+        if($SPGroup -eq $null)
+        {
+            $SPWeb.SiteGroups.Add($Group_Name, $Owner, $Default_Member, $Description)
+
+            $message = "Created group '" + $Group_Name + "'"
+            Write-To-Console -message $message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.INFO
+
+            $SPGroup = $SPWeb.SiteGroups[$Group_Name]
+        }
+    }
+    catch
+    {
+        $message = "Failed while creating group: '" + $Group_Name + "'. Error: '" + $_.Exception.Message + "'"
+        Write-To-Console -message $message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.ERROR
+    }
+
+    return $SPGroup
+}
+
+function Assign-Group-List-Permissions() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPList]
+        $SPList,
+
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPGroup]
+        $SPGroup,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Permission_Level
+    )
+
+    $Processed = $true;
+
+    try {
+        $SPWeb = Get-SPWeb -Identity $SPList.ParentWeb.Url
+
+        $SPRoleAssignement = New-Object Microsoft.SharePoint.SPRoleAssignment($SPGroup)
+        $SPRoleDefinition = $SPWeb.RoleDefinitions[$Permission_Level]
+        $SPRoleAssignement.RoleDefinitionBindings.Add($SPRoleDefinition)
+        $SPList.RoleAssignments.Add($SPRoleAssignement)
+        $SPList.Update()
+
+        $Message = "Assigned '" + $Permission_Level + "' to '" + $SPGroup.Name + "' on '" + $SPList.Title + "'"
+        Write-To-Console -message $Message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.INFO
+    }
+    catch {
+        $Processed = $false
+
+        $Message = "Error occured while assigning permissions on '" + $SPList.Title + "' " + " to: '" + $SPGroup.Name + "'. Error: " + $_.Exception.Message
+        Write-To-Console -message $Message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.ERROR 
+    }
+
+    return $Processed
+}
+
+function Assign-Group-Folder-Permission() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPFolder]
+        $SPFolder,
+
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPGroup]
+        $SPGroup,
+
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Permission_Level
+    )
+
+    $Processed = $true
+
+    try
+    {
+        $SPWeb = Get-SPWeb -Identity $SPFolder.ParentWeb.Url.ToString()
+
+        $SPRoleAssignement = New-Object Microsoft.SharePoint.SPRoleAssignment($SPGroup)
+        $SPRoleDefinition = $SPWeb.RoleDefinitions[$Permission_Level]
+        $SPRoleAssignement.RoleDefinitionBindings.Add($SPRoleDefinition)
+        $SPFolder.Item.RoleAssignments.Add($SPRoleAssignement)
+        $SPFolder.Item.Update()
+
+        $Message = "Assigned permissions on folder to group: '" + $SPGroup.Name + "'"
+        Write-To-Console -message $Message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.INFO
+    }
+    catch
+    {
+        $Processed = $false
+
+        $Message = "Failed to assign permissions on folder to group: '" + $SPGroup.Name + "'. Error: " + $_.Exception.Message
+        Write-To-Console -message $Message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.ERROR 
+    }
+
+    return $Processed
+}
+
+function Assign-User-Folder-Permission() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPFolder]
+        $SPFolder,
+
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPUser]
+        $SPUser,
+
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Permission_Level
+    )
+
+    $Processed = $true
+
+    try
+    {
+        $SPWeb = Get-SPWeb -Identity $SPFolder.ParentWeb.Url.ToString()
+
+        $SPRoleAssignement = New-Object Microsoft.SharePoint.SPRoleAssignment($SPUser)
+        $SPRoleDefinition = $SPWeb.RoleDefinitions[$Permission_Level]
+        $SPRoleAssignement.RoleDefinitionBindings.Add($SPRoleDefinition)
+        $SPFolder.Item.RoleAssignments.Add($SPRoleAssignement)
+        $SPFolder.Item.Update()
+
+        $Message = "Assigned permissions on folder to user: '" + $SPUser.DisplayName + "'"
+        Write-To-Console -message $Message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.INFO 
+    }
+    catch
+    {
+        $Processed = $false
+
+        $Message = "Failed to assign permissions on folder to user: '" + $SPUser.Email + "'. Error: " + $_.Exception.Message
+        Write-To-Console -message $Message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.ERROR 
+    }
+
+    return $Processed
+}
+
+function Remove-User-Folder-Permissions() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPFolder]
+        $SPFolder,
+
+        [Parameter(Mandatory = $true)]
+        [Microsoft.SharePoint.SPUser]
+        $SPUser
+    )
+
+    $Processed = $true;
+
+    try
+    {
+         $Role_Assignments = $SPFolder.Item.RoleAssignments
+
+         foreach($Role_Assignment in $Role_Assignments)
+         {
+            $Member = $Role_Assignment.Member;
+
+            if($Member.GetType().Name -eq "SPUser" -and $Member.Email -eq $SPUser.Email)
+            {
+                RemoveAll-RoleBindings -RoleAssignment $Role_Assignment
+            }
+         }
+    }
+    catch
+    {
+        $Processed = $false
+        $message = "Failed to remove folder permissions for user: '" + $SPUser.Email + "'"
+        Write-To-Console -message $message -section $CONFIG.LOGGING.SECTIONS.SP -category $CONFIG.LOGGING.CATEGORIES.ERROR
+    }
+
+    return $Processed
 }
 #>> END OF SP FUNCTIONS
